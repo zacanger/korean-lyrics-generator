5e39db9a9ae241036dad1b29442c7fe9a4dc7555
zacanger <zac@zacanger.com>
2022-11-12 20:15:14 -0700

    chore: init

---
+++ README.md
@@ -0,0 +1,21 @@
+# korean-lyrics-generator
+
+[![Support with PayPal](https://img.shields.io/badge/paypal-donate-yellow.png)](https://paypal.me/zacanger) [![Patreon](https://img.shields.io/badge/patreon-donate-yellow.svg)](https://www.patreon.com/zacanger) [![ko-fi](https://img.shields.io/badge/donate-KoFi-yellow.svg)](https://ko-fi.com/U7U2110VB)
+
+--------
+
+WIP. Original project: <https://github.com/baehyunsol/korean_saying_generator>
+
+## Installation
+
+Clone it
+
+## Usage
+
+`./gen.py`
+
+## TODO:
+
+Syllable count map
+
+[LICENSE](./LICENSE.md)
--- korean.py
+++ korean.py
@@ -23,46 +23,148 @@
 # ((ord(t) - 44032) % 588) % 28 -> 종성
 # 가각갂갃간갅갆갇갈갉갊갋갌갍갎갏감갑값갓갔강갖갗갘같갚갛
 
-cho = ('ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ')
-joong = ('ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ')
-jong = (None, 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ')
-ja = ('ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ')
+cho = (
+    "ㄱ",
+    "ㄲ",
+    "ㄴ",
+    "ㄷ",
+    "ㄸ",
+    "ㄹ",
+    "ㅁ",
+    "ㅂ",
+    "ㅃ",
+    "ㅅ",
+    "ㅆ",
+    "ㅇ",
+    "ㅈ",
+    "ㅉ",
+    "ㅊ",
+    "ㅋ",
+    "ㅌ",
+    "ㅍ",
+    "ㅎ",
+)
+joong = (
+    "ㅏ",
+    "ㅐ",
+    "ㅑ",
+    "ㅒ",
+    "ㅓ",
+    "ㅔ",
+    "ㅕ",
+    "ㅖ",
+    "ㅗ",
+    "ㅘ",
+    "ㅙ",
+    "ㅚ",
+    "ㅛ",
+    "ㅜ",
+    "ㅝ",
+    "ㅞ",
+    "ㅟ",
+    "ㅠ",
+    "ㅡ",
+    "ㅢ",
+    "ㅣ",
+)
+jong = (
+    None,
+    "ㄱ",
+    "ㄲ",
+    "ㄳ",
+    "ㄴ",
+    "ㄵ",
+    "ㄶ",
+    "ㄷ",
+    "ㄹ",
+    "ㄺ",
+    "ㄻ",
+    "ㄼ",
+    "ㄽ",
+    "ㄾ",
+    "ㄿ",
+    "ㅀ",
+    "ㅁ",
+    "ㅂ",
+    "ㅄ",
+    "ㅅ",
+    "ㅆ",
+    "ㅇ",
+    "ㅈ",
+    "ㅊ",
+    "ㅋ",
+    "ㅌ",
+    "ㅍ",
+    "ㅎ",
+)
+ja = (
+    "ㄱ",
+    "ㄲ",
+    "ㄳ",
+    "ㄴ",
+    "ㄵ",
+    "ㄶ",
+    "ㄷ",
+    "ㄸ",
+    "ㄹ",
+    "ㄺ",
+    "ㄻ",
+    "ㄼ",
+    "ㄽ",
+    "ㄾ",
+    "ㄿ",
+    "ㅀ",
+    "ㅁ",
+    "ㅂ",
+    "ㅃ",
+    "ㅄ",
+    "ㅅ",
+    "ㅆ",
+    "ㅇ",
+    "ㅈ",
+    "ㅉ",
+    "ㅊ",
+    "ㅋ",
+    "ㅌ",
+    "ㅍ",
+    "ㅎ",
+)
 mo = joong
 
 qwerty2kor = {
-	'q': 'ㅂ',
-	'Q': 'ㅃ',
-	'w': 'ㅈ',
-	'W': 'ㅉ',
-	'e': 'ㄷ',
-	'E': 'ㄸ',
-	'r': 'ㄱ',
-	'R': 'ㄲ',
-	't': 'ㅅ',
-	'T': 'ㅆ',
-	'y': 'ㅛ',  # don't forget qwerty2kor.get(x, qwerty2kor[x.lower()])
-	'u': 'ㅕ',
-	'i': 'ㅑ',
-	'o': 'ㅐ',
-	'O': 'ㅒ',
-	'p': 'ㅔ',
-	'P': 'ㅖ',
-	'a': 'ㅁ',
-	's': 'ㄴ',
-	'd': 'ㅇ',
-	'f': 'ㄹ',
-	'g': 'ㅎ',
-	'h': 'ㅗ',
-	'j': 'ㅓ',
-	'k': 'ㅏ',
-	'l': 'ㅣ',
-	'z': 'ㅋ',
-	'x': 'ㅌ',
-	'c': 'ㅊ',
-	'v': 'ㅍ',
-	'b': 'ㅠ',
-	'n': 'ㅜ',
-	'm': 'ㅡ'
+    "q": "ㅂ",
+    "Q": "ㅃ",
+    "w": "ㅈ",
+    "W": "ㅉ",
+    "e": "ㄷ",
+    "E": "ㄸ",
+    "r": "ㄱ",
+    "R": "ㄲ",
+    "t": "ㅅ",
+    "T": "ㅆ",
+    "y": "ㅛ",  # don't forget qwerty2kor.get(x, qwerty2kor[x.lower()])
+    "u": "ㅕ",
+    "i": "ㅑ",
+    "o": "ㅐ",
+    "O": "ㅒ",
+    "p": "ㅔ",
+    "P": "ㅖ",
+    "a": "ㅁ",
+    "s": "ㄴ",
+    "d": "ㅇ",
+    "f": "ㄹ",
+    "g": "ㅎ",
+    "h": "ㅗ",
+    "j": "ㅓ",
+    "k": "ㅏ",
+    "l": "ㅣ",
+    "z": "ㅋ",
+    "x": "ㅌ",
+    "c": "ㅊ",
+    "v": "ㅍ",
+    "b": "ㅠ",
+    "n": "ㅜ",
+    "m": "ㅡ",
 }
 
 kor2qwerty = dict((v, k) for k, v in qwerty2kor.items())
@@ -70,48 +172,57 @@ kor2qwerty = dict((v, k) for k, v in qwerty2kor.items())
 
 def divide(char):
 
-	ord_ = ord(char)
+    ord_ = ord(char)
 
-	if ord_ < 44032 or ord_ > 55203:
-		return [char]
+    if ord_ < 44032 or ord_ > 55203:
+        return [char]
 
-	ord_ -= 44032
+    ord_ -= 44032
 
-	return [cho[ord_ // 588], joong[(ord_ % 588) // 28], jong[(ord_ % 588) % 28]]
+    return [
+        cho[ord_ // 588],
+        joong[(ord_ % 588) // 28],
+        jong[(ord_ % 588) % 28],
+    ]
 
 
 def hasBatchim(char):
 
-	return 0 != ((ord(char) - 44032) % 588) % 28
+    return 0 != ((ord(char) - 44032) % 588) % 28
 
 
 # chars -> [cho, joong, jong]
 def combine(chars):
 
-	if len(chars) == 1:
-		return chars[0]
+    if len(chars) == 1:
+        return chars[0]
 
-	if type(chars) == tuple:
-		chars = list(chars)
+    if type(chars) == tuple:
+        chars = list(chars)
 
-	if len(chars) == 2:
-		chars.append(None)
+    if len(chars) == 2:
+        chars.append(None)
 
-	return chr(cho.index(chars[0]) * 588 + joong.index(chars[1]) * 28 + jong.index(chars[2]) + 44032)
+    return chr(
+        cho.index(chars[0]) * 588
+        + joong.index(chars[1]) * 28
+        + jong.index(chars[2])
+        + 44032
+    )
 
 
 def isKor(char):
 
-	ord_ = ord(char)
+    ord_ = ord(char)
 
-	if 44031 < ord_ < 55204:
-		return True
+    if 44031 < ord_ < 55204:
+        return True
 
-	if 12592 < ord_ < 12644:
-		return True
+    if 12592 < ord_ < 12644:
+        return True
 
-	return False
+    return False
 
 
 def forgotHanYoung(chars):
-	pass
+    pass
