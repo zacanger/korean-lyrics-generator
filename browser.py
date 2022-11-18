6d7d413435d1880781998f7637c796431f7f6f0d
zacanger <zac@zacanger.com>
2022-11-17 19:45:00 -0700

    fix: show errors

--- browser.py
+++ browser.py
@@ -8,8 +8,12 @@ output_paragraph = document["output"]
 
 def run_generation(_ev):
     input_lines = input_text.value.strip().split("\n")
-    results = logic.replace_pattern(logic.get_patterns(input_lines))
-    output_paragraph <= results
+    try:
+        results = logic.replace_pattern(logic.get_patterns(input_lines))
+        output_paragraph <= results
+    except:
+        output_paragraph.text = ""
+        output_paragraph <= "Error: check your input pattern format!"
 
 
 document["generate"].bind("click", run_generation)
